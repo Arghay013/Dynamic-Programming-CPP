49b966416e48e1116ed86a2488eb1c9e8b316b0b
Arghay013 <arghay.cse8.bu@gmail.com>
2024-09-16 21:12:08 +0600

    2nd

--- 0-1 KnapSack/KnapSack_memoization.cpp
+++ 0-1 KnapSack/KnapSack_memoization.cpp
@@ -1,19 +1,22 @@
 #include<bits/stdtr1c++.h>
 using namespace std;
+int t[102][1002];
 int KnapSack(int wt[], int val[], int W, int n){
     if(n==0 || W==0)return 0;
+    if(t[n][W] != -1)return t[n][W];
 
     if(wt[n-1] <= W){
-        return max(val[n-1] + KnapSack(wt, val, W-wt[n-1], n-1) , KnapSack(wt, val, W, n-1));
+        return t[n][W] = max(val[n-1] + KnapSack(wt, val, W-wt[n-1], n-1) , KnapSack(wt, val, W, n-1));
     }
     else if(wt[n-1] > W){
-        KnapSack(wt, val, W, n-1);
+       t[n][W] = KnapSack(wt, val, W, n-1);
     }
 }
 int main(){
     int wt[] ={ 10, 20, 30 } ;
     int val[] = { 60, 100, 120 };
     int W = 50;
+    memset(t, -1, sizeof(t));
     int n = sizeof(wt)/sizeof(wt[0]);
     cout<<KnapSack(wt, val, W, n)<<endl;
 
